7390080e62e3418d61a4e3560639780388a07862
elisa <elisasampietroterra@gmail.com>
2024-06-19 15:44:32 -0300

    Exercicio3lista2

---
+++ atividadesPythonElisa/Exercicio3lista2.py
@@ -0,0 +1,7 @@
+print("**********\n")
+print("        Calculo Horas       \n")
+print("**********\n")
+
+horas = int(input("\n Digite a quantidade de horas: "))
+
+print("A quantidade de horas em minutos é ", horas*60, "min")
