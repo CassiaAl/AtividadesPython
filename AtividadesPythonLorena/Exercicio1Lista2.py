10c1a68e948637fb671f9201d629d21eba5a513f
lozinha :) <arrudalorena64@gmail.com>
2024-06-19 15:33:31 -0300

    adiciona exercicio6Lista2.py

--- AtividadesPythonLorena/Exercicio1Lista2.py
+++
@@ -1,7 +0,0 @@
-peso = float(input("Digite seu peso em kg:"))
-
-if(peso <18.5):
-    print("Classificação: abaixo do peso")
-else:
-    print("classificação: peso normal")
-
---
+++ AtividadesPythonLorena/exercicio6Lista2.py
@@ -0,0 +1,6 @@
+anonascimento =int(input("digite o seu ano de nascimento: "))
+anoatual = int(input("digite o ano atual: "))
+
+idade = anoatual-anonascimento
+
+print(f"Voce tem {idade} : ")
