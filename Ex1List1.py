445137809d6851b378ec14033c0f201d553f1199
(Richard) <(richardepflores@gmail.com)>
2024-06-19 15:39:11 -0300

    Exercicio1lista1

---
+++ Ex1List1.py
@@ -0,0 +1,16 @@
+def main():
+    # Solicita o valor da base do retângulo
+    base = float(input("Digite o valor da base do retângulo: "))
+
+    # Solicita o valor da altura do retângulo
+    altura = float(input("Digite o valor da altura do retângulo: "))
+
+    # Calcula a área do retângulo
+    area = base * altura
+
+    # Exibe a área do retângulo
+    print(f"A área do retângulo é: {area}")
+
+
+if __name__ == "__main__":
+    main()
