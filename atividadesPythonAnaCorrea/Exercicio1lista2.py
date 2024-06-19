390ba866d8f43e1fe9a7c50a403252921354001a
Ana Correa <analiviaalmeidacorrea509@gmail.com>
2024-06-19 15:21:43 -0300

    adiciona exercicio 1 da lista 2

---
+++ atividadesPythonAnaCorrea/Exercicio1lista2.py
@@ -0,0 +1,13 @@
+main():
+    # Solicita ao usuário para inserir a base e a altura do retângulo
+    base = float(input("Digite o valor da base do retângulo: \n"))
+    altura = float(input("Digite o valor da altura do retângulo: \n"))
+
+    # Calcula a área do retângulo
+    area = base * altura
+
+    # Exibe o resultado
+    print(f"A área do retângulo é: {area}")
+
+if __name__ == "__main__":
+    main()
