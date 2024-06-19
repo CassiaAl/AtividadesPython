dc7761834b3e0836cceff8d46d390fb8e2f9ce98
lozinha :) <arrudalorena64@gmail.com>
2024-06-19 15:42:03 -0300

    adiciona exercicio3Lista2.py

---
+++ AtividadesPythonLorena/exercicio3Lista2.py
@@ -0,0 +1,5 @@
+horas = float(input("digite a quantidade de horas: "))
+
+minutos=horas*60
+
+print(f"{horas}horas equivalem a {minutos} minutos.")
--- AtividadesPythonLorena/exercicio6Lista2.py
+++
@@ -1,6 +0,0 @@
-anonascimento =int(input("digite o seu ano de nascimento: "))
-anoatual = int(input("digite o ano atual: "))
-
-idade = anoatual-anonascimento
-
-print(f"Voce tem {idade} : ")
