4b94e939cad251f6d8ee4e5f502023feb9b629ce
Thailany <thailany.gt.2007@icloud.com>
2024-06-19 14:43:48 -0300

    ajustar exercicio2 lista2

---
+++ AtividadesPythonThailany/Exercicio2Lista2.py
@@ -0,0 +1,8 @@
+print("**************************")
+print("     Programa Python      ")
+print("**************************")
+
+peso = float(input("\nInforme o numero do peso : \n"))
+altura = float(input("Informe o numero da altura :\n"))
+
+print("\nO valor do imc é de: \n", (altura*altura/peso))
