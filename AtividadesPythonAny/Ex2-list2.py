1547df0f9442f9d238f6b22cf4b28a56a2534eb4
Any Carolina <anycarolinagoehringe@gmail.com>
2024-06-19 16:11:19 -0300

    Ex4-list2 adicionado

--- AtividadesPythonAny/Ex2-list2.py
+++ AtividadesPythonAny/Ex2-list2.py
@@ -2,7 +2,7 @@ print("************")
 print("     CÁLCULO IMC     ")
 print("************")
 
-num1 = float(input("Digite o valor do seu peso:"))
-num2 = float(input("Digite o valor da altura:"))
+num1 = float(input("Digite o valor do seu peso: "))
+num2 = float(input("Digite o valor da altura: "))
 
-print ("O valor de seu IMC é:", num1/(num2*num2))
+print ("O valor de seu IMC é: ", num1/(num2*num2))
--- AtividadesPythonAny/Ex3-list2.py
+++ AtividadesPythonAny/Ex3-list2.py
@@ -2,6 +2,6 @@ print("************")
 print("     CÁLCULO HORAS PARA MINUTOS     ")
 print("************")
 
-num1 = float(input("Digite o valor das horas que deseja converter:"))
+num1 = float(input("Digite o valor das horas que deseja converter: "))
 
-print ("O valor de suas horas em minutos é:", num1 * 60)
+print ("O valor de suas horas em minutos é: ", num1 * 60)
