7c7692de7a27e89457aa02d8b9b03f814669f9a7
rayssa <yssapontes@gmail.com>
2024-06-19 15:16:38 -0300

    programa de soma-lista2

---
+++ AtividadesPythonRayssa/soma-lista2.py
@@ -0,0 +1,8 @@
+print("********************")
+print("  Programa Soma")
+print("********************")
+
+num1 = float(input("informe o primeiro número:  "))
+num2 = float(input("informe o segundo número:  "))
+
+print("o resultado da soma é", num1 + num2)
