205314e17ea7256d8330c6f17e899a7dc0104c59
vitor <vitorglrps@gmail.com>
2024-06-19 15:18:53 -0300

    adicionar exercicio1 lista2.py

---
+++ AtividadesPython/atividadesPythonVitor/exercicio1.py
@@ -0,0 +1,8 @@
+print("**********")
+print("     Programa Python      ")
+print("**********")
+
+base = int(input("\nInforme o numero da base : \n"))
+altura = int(input("Informe o numero da altura :\n"))
+
+print("\nO valor da área é de:", base*altura)
