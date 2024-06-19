5f9540b87ec4d13351a697ec8e7ee6220d3f99e2
Thailany <thailany.gt.2007@icloud.com>
2024-06-19 14:16:50 -0300

    ajustar exercicio1 lista2

--- AtividadesPythonThailany/Exercicio1Lista2.py
+++ AtividadesPythonThailany/Exercicio1Lista2.py
@@ -1,1 +1,8 @@
-print("teste")
+print("**************************")
+print("     Programa Python      ")
+print("**************************")
+
+base = int(input("\nInforme o numero da base : \n"))
+altura = int(input("Informe o numero da altura :\n"))
+
+print("\nO valor da área é de:", base*altura)
