be0d2c3ef993a5e6ec42894065beb0f983b28dfb
vitor <vitorglrps@gmail.com>
2024-06-19 15:43:42 -0300

    exercico4 lista2.py

--- AtividadesPython/atividadesPythonVitor/exercicio3lista2.py
+++ AtividadesPython/atividadesPythonVitor/exercicio3lista2.py
@@ -1,1 +1,7 @@
-git
+print("**********\n")
+print("        Calculo Horas       \n")
+print("**********\n")
+
+horas = int(input("\n Digite a quantidade de horas: "))
+
+print("A quantidade de horas em minutos é ", horas*60, "min")
