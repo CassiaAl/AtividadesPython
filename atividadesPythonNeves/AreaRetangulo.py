82a8a3eea1f7ec142a351002acf2c02964486189
Anna Neves <annalivianeves1303@gmail.com>
2024-06-24 11:16:29 -0300

    Ex Área Retângular

---
+++ atividadesPythonNeves/AreaRetangulo.py
@@ -0,0 +1,6 @@
+base = float(input("insira o valor da Base: "))
+altura = float(input("insira o valor da Altura: "))
+print("A Base digitada foi ", base," e a Altura foi de", altura)
+print (" ")
+print("o resultado da area é", base*altura)
+
