4a5987690368f858de24fb8752096716a7697ac8
Thailany <thailany.gt.2007@icloud.com>
2024-06-24 09:56:09 -0300

    Exercicio6Lista2 ajustar

--- AtividadesPythonThailany/Exercicio6Lista2.py
+++ AtividadesPythonThailany/Exercicio6Lista2.py
@@ -2,7 +2,7 @@ print("********************")
 print("    Ano bissexto")
 print("********************")
 
-ano = int(input("Digite um ano para verificar se o meso é bissexto: "))
+ano = int(input("Digite um ano para verificar se o mesmo é bissexto: "))
 
 if ano % 4 == 0:
     print("O ano " ,ano, " é bissexto")
