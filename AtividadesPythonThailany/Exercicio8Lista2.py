cf167461adebd0c3957e7a225774f6410a71379a
Thailany <thailany.gt.2007@icloud.com>
2024-06-24 16:02:23 -0300

    Ajustar Exercicio8Lista2

--- AtividadesPythonThailany/Exercicio8Lista2.py
+++ AtividadesPythonThailany/Exercicio8Lista2.py
@@ -2,7 +2,7 @@ print("******************")
 print("   Par ou Ímpar")
 print("******************")
 
-num = int(input("Digite um número para verificar se ele é primo: "))
+num = int(input("Digite um número: "))
 
 if num % 2 == 0:
     print("O número ",num," é par")
