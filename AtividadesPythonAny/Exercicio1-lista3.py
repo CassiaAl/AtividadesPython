99b6ba26ab40d683ccd4bd3190bc37f2ef13405d
Any Carolina <anycarolinagoehringe@gmail.com>
2024-06-24 09:01:03 -0300

    exercicio2-lista3 add

--- AtividadesPythonAny/Exercicio1-lista3.py
+++ AtividadesPythonAny/Exercicio1-lista3.py
@@ -5,8 +5,8 @@ print("************")
 conver = str(input("Informe se a conversão será para dólar ou euro:  "))
 num = float(input("informe o valor para a conversão:  "))
 
-if (conver == 1):
+if conver == 1:
     print("A conversão de real para dólar é:  ", num/5.43)
 
-elif (conver == 2):
+elif conver == 2:
     print("A conversão de real para euro é:  ", num/5.83)
