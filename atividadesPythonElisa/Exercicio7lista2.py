be10e6c3db1e546e8bef935a4be5276a3e9146bb
elisa <elisasampietroterra@gmail.com>
2024-06-24 11:36:02 -0300

    Exercicio7lista2

---
+++ atividadesPythonElisa/Exercicio7lista2.py
@@ -0,0 +1,9 @@
+print("*******")
+print("  Ano de nascimento")
+print("*******")
+
+anonasc = int(input("Digite o ano de nascimento: "))
+
+anoatu = int(input("Digite o ano atual: "))
+
+print("Digite a sua idade é ",anoatu-anonasc," ano/anos")
