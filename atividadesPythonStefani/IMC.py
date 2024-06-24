2da587bf1c4f864728332290ca212d940ab2f033
Sté ;) <stefanifriaspires691@gmail.com>
2024-06-24 09:15:48 -0300

    exer1-lista2 add

--- atividadesPythonStefani/IMC.py
+++
@@ -1,8 +0,0 @@
-print("********************************")
-print("     CÁLCULO IMC     ")
-print("********************************")
-
-num1 = float(input("Digite o valor do seu peso:"))
-num2 = float(input("Digite o valor da altura:"))
-
-print ("O valor de seu IMC é:", num1/(num2*num2))
--- atividadesPythonStefani/exer1-lista2.py
+++ atividadesPythonStefani/exer1-lista2.py
@@ -2,7 +2,7 @@ print("********************************")
 print("     ÁREA DE UM RETÂNGULO      ")
 print("********************************")
 
-num1 = int(input("Digite o valor de sua base:"))
+num1 = int(input("Digite o valor de seu :"))
 num2 = int(input("Digite o valor da altura:"))
 
 print ("O valor da área de seu retângulo é:", num1 * num2)
