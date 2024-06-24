48ce1cac7053428cf37cd2206d5aa51371d2f264
(Richard) <(richardepflores@gmail.com)>
2024-06-24 09:52:43 -0300

    Ex2list1

---
+++ Ex2list1.py
@@ -0,0 +1,13 @@
+def main():
+    try:
+        massa = float(input("Digite o peso em kg: "))
+        altura = float(input("Digite a altura em metros: "))
+        imc = massa / (altura ** 2)
+        print(f"O IMC é: {imc:.2f}")
+    except ValueError:
+        print("Entrada inválida. Certifique-se de inserir valores numéricos válidos.")
+
+if __name__ == "__main__":
+    main()
+
+
