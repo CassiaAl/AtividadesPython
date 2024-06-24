8f65d2d0c3fd5c0e560b9a9a5d3400f1d3dbbba6
lozinha :) <arrudalorena64@gmail.com>
2024-06-24 08:10:41 -0300

    adiciona exercicio

---
+++ AtividadesPythonLorena/exercicio9lista3.py
@@ -0,0 +1,10 @@
+num1 = int(input("digite o primeiro numero inteiro:"))
+num2 = int(input("digite o segundo numero inteiro :"))
+
+maior = max(num1, num2)
+menor = min(num1, num2)
+
+diferenca=maior-menor
+
+print(f"a diferenca entre o maior e o menor valor é {diferenca}")
+
