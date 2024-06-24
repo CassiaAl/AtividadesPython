04cf26d6f39cbc1df9fe71b760de50c02dd40020
Bruna <bf3562983@gmail.com>
2024-06-24 11:46:09 -0300

    adiciona exercicios

---
+++ atividadesPythonBruna/valor acumulado.py
@@ -0,0 +1,13 @@
+
+acumulado = int(0)
+
+while (acumulado<=100):
+    numero = (int(input('Digite um valor: ')))
+    acumulado = acumulado + numero
+    print('Valor acumulado: ' ,acumulado,)
+
+else:
+    print('Valor acumulado ou ultrapassou o valor de 100')
+
+
+
