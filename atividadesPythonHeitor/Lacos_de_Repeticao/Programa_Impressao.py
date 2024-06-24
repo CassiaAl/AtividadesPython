73ce5587366923222df4cb37546d0243bac58f6c
heitor-murbak <he1tor.murb4k@gmail.com>
2024-06-24 11:19:07 -0300

    Exercicio 1 Lacos de Repeticao

---
+++ atividadesPythonHeitor/Lacos_de_Repeticao/Programa_Impressao.py
@@ -0,0 +1,8 @@
+print("**********************")
+print("  Programa Impressão  ")
+print("**********************")
+
+num = int(input("Digite um número inteiro: "))
+
+for i in range(1, num+1):
+    print(i, end=" ")
