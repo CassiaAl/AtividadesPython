0cb8eebd39d1a0741a04440f2ff9b5dc30405533
Thailany <thailany.gt.2007@icloud.com>
2024-06-24 09:36:51 -0300

    Exercicio5Lista2 alterar

--- AtividadesPythonThailany/Exercicio5Lista2.py
+++ AtividadesPythonThailany/Exercicio5Lista2.py
@@ -2,7 +2,7 @@ print("******************")
 print("   Número Primo")
 print("******************\n")
 
-numprimo = int(input("Digite o número que deseja verificar se é primo: "))
+numprimo = int(input("Digite o número que deseja verificar se é primo:\n "))
 
 if numprimo % 6 == 0 or numprimo % 4 == 0 or numprimo % 10 == 0 or numprimo % 9 == 0:
     print("O número ",numprimo," não é primo")
