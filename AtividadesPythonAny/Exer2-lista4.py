f442a750fc01a96415a258a8bcc6c3bbd06e591f
Any Carolina <anycarolinagoehringe@gmail.com>
2024-06-24 11:14:00 -0300

    Exer2-lista4 add

---
+++ AtividadesPythonAny/Exer2-lista4.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número para ver sua tabuada: "))
+
+i = 1
+
+print(f"\nTabuada de {numero}:")
+while i <= 10:
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+    i += 1
