d689bf335c4f96d4d50c27989be9227a75d63848
Thaila <thailartavera@gmail.com>
2024-06-24 11:56:11 -0300

    adiciona positivoOuNegativoExercicio3.py

---
+++ atividadesPythonThaila/positivoOuNegativoExercicio3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o número desejado: "))
+if num1 > 0:
+    print("O número digitado é positivo")
+if num1 < 0:
+    print("O número digitado é negativo")
+if num1 == 0:
+    print("O número digitado é nulo")
