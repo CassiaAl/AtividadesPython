fee159d31ca2658a553d3f48f247e032cde56083
lozinha :) <arrudalorena64@gmail.com>
2024-06-24 10:48:54 -0300

    Exercicio1Lista2.py

--- AtividadesPythonLorena/Exercicio2Lista4.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Digite um número inteiro positivo: "))
-
-if n > 0:
-    soma = 0
-    print(f"A soma de todos os números de 1 a N é soma: {n}")
-else:
-    print("Número inválido! Digite um número inteiro positivo")
