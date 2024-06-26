b20fb10ec2ad1ae093d4020ba42d536c8cac2e5a
Anna Neves <annalivianeves1303@gmail.com>
2024-06-26 09:31:42 -0300

    Ex Ímpar e Par

---
+++ atividadesPythonNeves/ImparePar.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um Número: "))
+print("O Número escolhido foi", num)
+print("")
+if num % 2 == 0:
+    print("O Número", num, "é par! :)")
+else:
+    print("O Número", num, "é ímpar! :)")
