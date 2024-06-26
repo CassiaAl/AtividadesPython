99dcf755f3b9f49c113123b47339ae149a5b44cd
Ana Julia <anajulia.pedroso7@gmail.com>
2024-06-26 10:05:10 -0300

    adicion converterhorasExercicio.py

---
+++ AtividadesPythonAnaJulia/calculoimcExercicio.py
@@ -0,0 +1,4 @@
+print("Programa IMC")
+peso = float(input("digite seu peso: "))
+altura = float(input("digite sua altura: "))
+print("O seu IMC é: ", peso/altura**altura)
---
+++ AtividadesPythonAnaJulia/converterhorasExercicio.py
@@ -0,0 +1,3 @@
+print("Programa Converter hora em minuto")
+hora = float(input("Digite a hora: "))
+print("Os minutos são: ", hora*60)
