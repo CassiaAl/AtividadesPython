cb422446a5a4fa7c9b388341f6e659a8c3dbc85e
CassiaAl <39555169+CassiaAl@users.noreply.github.com>
2024-06-26 11:39:38 -0300

    Delete Conversão.py

--- Conversão.py
+++
@@ -1,9 +0,0 @@
-print ("--------------------------")
-print ("     HORAS EM MINUTOS     ")
-print ("--------------------------")
-
-hora = int(input("Digite a quantidade de horas: "))
-
-resultado = hora * 60
-
-print ("Os minutos são: ", resultado)
